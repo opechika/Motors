588b2342b817190310e5c40ae33ef2f7867af7d1
opechika <opechika@yahoo.com>
2019-02-05 21:35:21 +0000

    Committing Page Object Model Work

---
+++ MotorsProject/Pages/SearchResultPage.cs
@@ -0,0 +1,29 @@
+﻿using MotorsProject.Helper;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorsProject.Pages
+{
+   public class SearchResultPage : BaseClass
+    {
+        IList<IWebElement> SearchResult;
+
+
+        public void IsSearchResultDisplayed(string sResult)
+        {
+
+            SearchResult = GetElementsByCssSelector("h3.title");
+
+            foreach (IWebElement result in SearchResult)
+            {
+                var resultText = result.Text;
+                Assert.True(resultText.Contains(sResult));
+            }
+        }
+    }
+}
